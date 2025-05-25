1af55832662b8fd6842ee5cc8386388e150c7e93
ifs-abhilashkallampunathi <abhilash.kallam.punathi@ifs.com>
2025-05-25 21:37:23 +0800

    code update

--- write_jobhistory_to_adls.py
+++ write_jobhistory_to_adls.py
@@ -17,16 +17,20 @@ def set_sas_token(spark, sas_token, storage_account, container):
     # "fs.azure.sas.table-maint-job-results.bmdatalaketest.dfs.core.windows.net",
     # decoded_token
     # )
-    spark.conf.set("fs.azure.account.auth.type.bmdatalaketest.dfs.core.windows.net", "SAS")
-    spark.conf.set("fs.azure.sas.token.provider.type.bmdatalaketest.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.sas.FixedSASTokenProvider")
-    spark.conf.set("fs.azure.sas.fixed.token.bmdatalaketest.dfs.core.windows.net", formatted_token)
-    # spark.conf.set(f"fs.azure.account.auth.type.{storage_account}.dfs.core.windows.net", "OAuth")
-    # spark.conf.set(f"fs.azure.account.oauth.provider.type.{storage_account}.dfs.core.windows.net",
-    #            "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
-    # spark.conf.set(f"fs.azure.account.oauth2.client.id.{storage_account}.dfs.core.windows.net", resolved_cid)
-    # spark.conf.set(f"fs.azure.account.oauth2.client.secret.{storage_account}.dfs.core.windows.net", resolved_cpwd)
-    # spark.conf.set(f"fs.azure.account.oauth2.client.endpoint.{storage_account}.dfs.core.windows.net",
-    #            f"https://login.microsoftonline.com/{resolved_tid}/oauth2/token")
+    # spark.conf.set("fs.azure.account.auth.type.bmdatalaketest.dfs.core.windows.net", "SAS")
+    # spark.conf.set("fs.azure.sas.token.provider.type.bmdatalaketest.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.sas.FixedSASTokenProvider")
+    # spark.conf.set("fs.azure.sas.fixed.token.bmdatalaketest.dfs.core.windows.net", formatted_token)
+
+    resolved_cid: str = '695ae555-406e-41f4-93c1-5b85d68c5009'
+    resolved_cpwd: str = 'abAF2y_UEl2_aT5lj332~Cz.9_etM9HF8.'
+    resolved_tid: str = 'afadec18-0533-4cba-8578-5316252ff93f'
+    spark.conf.set(f"fs.azure.account.auth.type.{storage_account}.dfs.core.windows.net", "OAuth")
+    spark.conf.set(f"fs.azure.account.oauth.provider.type.{storage_account}.dfs.core.windows.net",
+               "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
+    spark.conf.set(f"fs.azure.account.oauth2.client.id.{storage_account}.dfs.core.windows.net", resolved_cid)
+    spark.conf.set(f"fs.azure.account.oauth2.client.secret.{storage_account}.dfs.core.windows.net", resolved_cpwd)
+    spark.conf.set(f"fs.azure.account.oauth2.client.endpoint.{storage_account}.dfs.core.windows.net",
+               f"https://login.microsoftonline.com/{resolved_tid}/oauth2/token")
     # print(f"✅ Set Spark Hadoop config {conf_key}")
 
 def main():
