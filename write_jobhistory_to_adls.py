e39a11009a92df503442b57275bd94202c300396
ifs-abhilashkallampunathi <abhilash.kallam.punathi@ifs.com>
2025-05-26 00:15:12 +0800

    code update

--- write_jobhistory_to_adls.py
+++ write_jobhistory_to_adls.py
@@ -8,7 +8,7 @@ from pyspark.sql import SparkSession
 
 def set_sas_token(spark, sas_token, storage_account, container):
     decoded_token = urllib.parse.unquote(sas_token)
-    # formatted_token = f"?{decoded_token}"
+    formatted_token = f"?{decoded_token}"
     print(f"🔑 Decoded SAS token: {decoded_token}")
 
     # conf_key = f"fs.azure.sas.{container}.{account}.dfs.core.windows.net"
@@ -23,7 +23,7 @@ def set_sas_token(spark, sas_token, storage_account, container):
 
     spark.conf.set(f"fs.azure.account.auth.type.{storage_account}.dfs.core.windows.net", "SAS")
     spark.conf.set(f"fs.azure.sas.token.provider.type.{storage_account}.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.sas.FixedSASTokenProvider")
-    spark.conf.set(f"fs.azure.sas.{container}.{storage_account}.dfs.core.windows.net", sas_token)
+    spark.conf.set(f"fs.azure.sas.{container}.{storage_account}.dfs.core.windows.net", formatted_token)
 
 
     # resolved_cid: str = '695ae555-406e-41f4-93c1-5b85d68c5009'
